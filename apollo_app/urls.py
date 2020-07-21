9da5ad569b8c161b68683773f90432a07af3c2fd
enen112233 <68358100+enen112233@users.noreply.github.com>
2020-07-21 16:28:12 +0800

    合并特性分支B到develop分支上

--- apollo_app/urls.py
+++ apollo_app/urls.py
@@ -6,6 +6,10 @@ from apollo_app import views
 urlpatterns = [
     path('index/',views.index),
     path('login/', views.login),
+    path('safe/',views.safeA),
+    path('safe/',views.safeA),
+    path('login/', views.login),
+    path('safe_b/', views.safe_b),
 ]
 
 
--- apollo_app/views.py
+++ apollo_app/views.py
@@ -10,3 +10,10 @@ def index(request):
 def login(request):
     return HttpResponse("login")
 
+def safe_b(request):
+    return HttpResponse("实现了安全等级为b的级别")
+
+def safeA(request):
+    return HttpResponse("实现了A级别的安全等级")
+
+
